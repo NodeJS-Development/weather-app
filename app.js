c2ca9822badaa0fa79295249f2e73bf3d13a1d2f
Brayan Garcia <bgarcia.95@outlook.com>
2022-12-22 10:05:19 -0600

    remove unused import. create customized http request.

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
-const { url } = require('inspector');
 const request = require('request');
 
-const URL = 'http://api.weatherstack.com/current?access_key=8d70e6eac36e20855960ad9352fb67a3&query=37.6267,-122.4233';
+const URL = 'http://api.weatherstack.com/current?access_key=8d70e6eac36e20855960ad9352fb67a3&query=37.6267,-122.4233&units=f';
 
-request({ url: URL }, (error, response) => {
-  const data = JSON.parse(response.body);
-
-  console.log(data.current);
+request({ url: URL, json: true,  }, (error, response) => {
+  const {temperature, feelslike, weather_descriptions } = response.body.current;
+  console.log(`${weather_descriptions[0]}. It is currently ${temperature} degrees out. It feels like ${feelslike} degrees out. `);
 });
+
+// Goal: Print a small forecast to the user
+//
+// 1. Print "It is currently 58.55 degrees out. It feels like 5 degrees out."
+// 2. Test your work!
